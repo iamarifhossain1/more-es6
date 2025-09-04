44d622f3ea840e984d1a9139047f8e8a4d08a134
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-09-05 00:17:07 +0600

    this

--- this.js
+++ this.js
@@ -1,12 +1,18 @@
 class Vehicle {
+  #tin;
   constructor(type, brand, price) {
     this.type = type;
     this.brand = brand;
     this.price = price;
+    this.#tin = "152641125";
   }
 
   getThis() {
     console.log(this);
+  }
+
+  getTin() {
+    return this.#tin;
   }
 
   getPrice() {
@@ -14,5 +20,26 @@ class Vehicle {
   }
 }
 
-const car = new Vehicle("car", "Toyota", 5000);
-console.log(car);
+const car1 = new Vehicle("Car", "Toyota", 500000);
+const car2 = new Vehicle("Car", "Mitshubishi", 1000000);
+
+console.log(car1.getPrice());
+// console.log(car2.getPrice());
+
+// const students = {
+//   name: "Arif hossain",
+//   age: 28,
+//   batch: "12th",
+//   location: "Mirpur",
+//   Country: "Bangladesh",
+//   getOutput: function () {
+//     console.log(this);
+//   },
+
+//   getArrow: () => {
+//     console.log(this);
+//   },
+// };
+
+// students.getOutput();
+// students.getArrow();
